0a4fd76fd24c58558a4e14176bd39b780fac2b50
Cristian Perez <cperezlecaros@gmail.com>
2025-08-22 12:12:44 -0400

    Add exceptions and global exception handler

---
+++ src/main/java/inventory/system/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package inventory.system.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
