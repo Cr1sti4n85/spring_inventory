e926009b6eedd98f4359a45f95dc29f000267df2
Cristian Perez <cperezlecaros@gmail.com>
2025-08-25 10:29:42 -0400

    Add User Service interface

---
+++ src/main/java/inventory/system/services/IUserService.java
@@ -0,0 +1,26 @@
+package inventory.system.services;
+
+import inventory.system.dto.LoginRequest;
+import inventory.system.dto.Response;
+import inventory.system.dto.SignupRequest;
+import inventory.system.dto.UserDTO;
+import inventory.system.models.User;
+
+public interface IUserService {
+
+    Response registerUser(SignupRequest signupRequest);
+
+    Response loginUser(LoginRequest loginRequest);
+
+    Response getAllUsers();
+
+    User getCurrentLoggedInUser();
+
+    Response getUserById(Long id);
+
+    Response updateUser(Long id, UserDTO userDTO);
+
+    Response deleteUser(Long id);
+
+    Response getUserTransactions(Long id);
+}
---
+++ src/main/java/inventory/system/services/implementation/UserService.java
@@ -0,0 +1,4 @@
+package inventory.system.services.implementation;
+
+public class UserService {
+}
