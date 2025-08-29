60e35dc8dda81dd96ddde94e011faa584fd93f34
Cristian Perez <cperezlecaros@gmail.com>
2025-08-29 12:11:16 -0400

    Create Product Controller

---
+++ src/main/java/inventory/system/controllers/ProductController.java
@@ -0,0 +1,90 @@
+package inventory.system.controllers;
+
+import inventory.system.dto.ProductDTO;
+import inventory.system.dto.Response;
+import inventory.system.services.implementation.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> saveProduct(
+            @RequestParam("imageFile") MultipartFile imageFile,
+            @RequestParam("name") String name,
+            @RequestParam("sku") String sku,
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("stock") Integer stockQuantity,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestParam(value = "description", required = false) String description
+    ) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName(name);
+        productDTO.setSku(sku);
+        productDTO.setPrice(price);
+        productDTO.setStock(stockQuantity);
+        productDTO.setCategoryId(categoryId);
+        productDTO.setDescription(description);
+
+        return ResponseEntity.ok(productService.saveProduct(productDTO, imageFile));
+
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> updateProduct(
+            @RequestParam(value = "imageFile", required = false) MultipartFile imageFile,
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "sku", required = false) String sku,
+            @RequestParam(value = "price", required = false) BigDecimal price,
+            @RequestParam(value = "stock", required = false) Integer stockQuantity,
+            @RequestParam(value = "categoryId", required = false) Long categoryId,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam("productId") Long productId
+    ) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName(name);
+        productDTO.setSku(sku);
+        productDTO.setPrice(price);
+        productDTO.setProductId(productId);
+        productDTO.setStock(stockQuantity);
+        productDTO.setCategoryId(categoryId);
+        productDTO.setDescription(description);
+
+        return ResponseEntity.ok(productService.updateProduct(productDTO, imageFile));
+
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Response> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> deleteProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.deleteProduct(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Response> searchProduct(@RequestParam String input) {
+        return ResponseEntity.ok(productService.searchProduct(input));
+    }
+}
